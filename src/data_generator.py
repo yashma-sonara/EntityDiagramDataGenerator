d36b0a28f86ce4448ba7b4721d050222b3dd6982
Wongqingyee <qingyeewong@gmail.com>
2026-04-07 00:52:51 +0800

    Add logic for generating data for relationship sets

--- src/data_generator.py
+++ src/data_generator.py
@@ -2,7 +2,7 @@ from faker import Faker
 import numpy as np
 import random
 
-from .schema import Distribution, SchemaSpec, EntitySpec, AttributeSpec
+from .schema import Distribution, SchemaSpec, EntitySpec, AttributeSpec, RelationshipSpec
 
 CUSTOM_GENERATORS: dict = {
     # --- Academic ---
@@ -169,5 +169,213 @@ def generate_dataset(spec: SchemaSpec) -> dict[str, list[dict]]:
 
     for entity in spec.entities:
         datasets[entity.name] = generate_entity(entity, fake)
+    
+    relationship_data = generate_relationship(spec, datasets)
+
+    datasets.update(relationship_data)
 
     return datasets
+
+
+def generate_rel_attrs(rel: RelationshipSpec) -> dict:
+    # Uses the same logic as generate_value for attributes on the relationship itself
+    fake = Faker()
+    return {attr.name: generate_value(attr, fake) for attr in rel.attributes}
+
+
+def should_use_table(rel):
+    return rel.type == "many_to_many" or len(rel.attributes) > 0
+
+
+def gen_one_to_one_pairs(ids_a, ids_b, part_a, part_b):
+    A = list(ids_a)
+    B = list(ids_b)
+
+    random.shuffle(A)
+    random.shuffle(B)
+
+    pairs = []
+
+    # Case 1: total-total
+    # Every A must map to one B AND every B must map to one A
+    if part_a == "total" and part_b == "total":
+        if len(A) != len(B):
+            raise ValueError("1:1 total-total requires equal sizes")
+        pairs = list(zip(A, B))
+
+    # Case 2: A total, B partial
+    # Every A must map to some B, but some B can be unmapped
+    elif part_a == "total":
+        if len(B) < len(A):
+            raise ValueError("Not enough B for A total")
+        pairs = [(A[i], B[i]) for i in range(len(A))]
+
+    # Case 3: B total, A partial
+    # Every B must map to some A, but some A can be unmapped
+    elif part_b == "total":
+        if len(A) < len(B):
+            raise ValueError("Not enough A for B total")
+        pairs = [(A[i], B[i]) for i in range(len(B))]
+
+    # Case 4: partial-partial
+    # random subset of unique pairs
+    else:
+        k = random.randint(0, min(len(A), len(B)))
+        pairs = list(zip(random.sample(A, k), random.sample(B, k)))
+
+    return pairs
+
+
+def gen_one_to_many_pairs(ids_a, ids_b, part_a, part_b):
+    # A = one side, B = many side
+    pairs = []
+    used_b = set()
+    B = list(ids_b)
+
+    # Case 1: B total
+    # Every B must map to exactly one A
+    if part_b == "total":
+        for b in ids_b:
+            a = random.choice(ids_a)
+            pairs.append((a, b))
+            used_b.add(b)
+
+    # Case 2: A total
+    # Every A must appear at least once
+    if part_a == "total":
+        used_a = {a for a, _ in pairs}
+        for a in ids_a:
+            if a not in used_a:
+                # Find a B that has not been assigned yet to maintain 1:N integrity
+                available_b = [b for b in B if b not in used_b]
+
+                if not available_b:
+                    raise ValueError("Not enough B to satisfy A total")
+                
+                b = random.choice(available_b)
+                pairs.append((a, b))
+                used_b.add(b)
+
+    # Fill the rest of the relationships randomly based on B's capacity
+    remaining_b = [b for b in B if b not in used_b]
+    extra = random.randint(0, len(ids_b))
+    for _ in range(extra):
+        if not remaining_b:
+            break
+        
+        a = random.choice(ids_a)
+        b = random.choice(remaining_b)
+        remaining_b.remove(b)
+        pairs.append((a, b))
+
+    return pairs
+
+
+def gen_many_to_many_pairs(ids_a, ids_b, part_a, part_b, target_rows):
+    pairs = []
+    used_pairs = set()
+
+    # CASE 1: A total
+    # Every A must appear at least once
+    if part_a == "total":
+        for a in ids_a:
+            b = random.choice(ids_b)
+            pair = (a, b)
+            if pair not in used_pairs:
+                used_pairs.add(pair)
+                pairs.append(pair)
+
+    # CASE 2: B total 
+    # Every B must appear at least once
+    if part_b == "total":
+        used_b = {b for _, b in pairs}
+        for b in ids_b:
+            if b not in used_b:
+                a = random.choice(ids_a)
+                pair = (a, b)
+                if pair not in used_pairs:
+                    used_pairs.add(pair)
+                    pairs.append(pair)
+
+    # Fill until we reach the user specified row count
+    while len(pairs) < target_rows:
+        a = random.choice(ids_a)
+        b = random.choice(ids_b)
+        pair = (a, b)
+
+        if pair not in used_pairs:
+            used_pairs.add(pair)
+            pairs.append(pair)
+
+    return pairs
+
+
+def assign_fk(pairs, entity_a, entity_b, pk_a,pk_b, datasets):
+    fk_name = entity_a.lower() + '_' + pk_a
+
+    # Initialize all rows with None (to handle partial participation)
+    for row in datasets[entity_b]:
+        row[fk_name] = None
+
+    # Assign FK values based on generated pairs
+    for a, b in pairs:
+        for row in datasets[entity_b]:
+            if row[pk_b] == b: # find matching B row
+                row[fk_name] = a
+                break
+
+
+def build_relationship_table(pairs, pk_a, pk_b, rel):
+    rows = []
+    for a, b in pairs:
+        rows.append({
+            pk_a: a,
+            pk_b: b,
+            **generate_rel_attrs(rel)
+        })
+    return rows
+
+
+def generate_relationship(spec, datasets):
+    relationship_data = {}
+
+    for rel in spec.relationships:
+        entity_a, entity_b = rel.between
+
+        pk_a = spec.entity_map()[entity_a].primary_key().name
+        pk_b = spec.entity_map()[entity_b].primary_key().name
+
+        ids_a = [row[pk_a] for row in datasets[entity_a]]
+        ids_b = [row[pk_b] for row in datasets[entity_b]]
+
+        part_a = rel.participation.get(entity_a)
+        part_b = rel.participation.get(entity_b)
+
+        # Only create table for relationship set that has attributes
+        # and cardinality constraints of many_to_many
+        use_table = should_use_table(rel)
+
+        if rel.type == "one_to_one":
+            pairs = gen_one_to_one_pairs(ids_a, ids_b, part_a, part_b)
+
+        elif rel.type == "one_to_many":
+            pairs = gen_one_to_many_pairs(ids_a, ids_b, part_a, part_b)
+
+        elif rel.type == "many_to_many":
+            target_rows = rel.rows
+            pairs = gen_many_to_many_pairs(ids_a, ids_b, part_a, part_b, target_rows)
+
+
+        if use_table:
+            relationship_data[rel.name] = build_relationship_table(pairs, pk_a, pk_b, rel)
+
+        # Update the datasets for the foreign key column
+        else:
+            if rel.type == "one_to_one":
+                assign_fk(pairs, entity_a, entity_b, pk_a, pk_b, datasets)
+
+            elif rel.type == "one_to_many":
+                assign_fk(pairs, entity_a, entity_b, pk_a, pk_b, datasets)
+
+    return relationship_data
+
